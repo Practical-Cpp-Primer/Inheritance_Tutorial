427c94c9462ff4c3ee58cdd6a36116335ae6e5c7
DarrienG <darrienglasser@outlook.com>
2016-07-16 16:52:36 -0400

    Implementation with unique pointers

--- src/main.cpp
+++ src/main.cpp
@@ -4,52 +4,59 @@
 #include "Pancakes.h"
 
 #include <iostream>
+#include <memory>
 #include <vector>
 
 using std::cout;
 using std::endl;
 using std::vector;
+using std::shared_ptr;
+using std::unique_ptr;
 
-void prepare(vector<Food*> itemsToCook);
-void checkPreparedness(vector<Food*> itemsToCook);
+vector<unique_ptr<Food>> prepare(vector<unique_ptr<Food>> itemsToCook);
+vector<unique_ptr<Food>> checkPreparedness(vector<unique_ptr<Food>> itemsToCook);
 
 int main(int argc, char const *argv[]){
 	// Initialize foods
-	Bananas banana;
-	Bread bread;
-	Eggs eggs;
-	Pancakes pancakes;
+	unique_ptr<Bananas> banana(new Bananas);
+	unique_ptr<Bread> bread(new Bread);
+	unique_ptr<Eggs> eggs(new Eggs);
+	unique_ptr<Pancakes> pancakes(new Pancakes);
 	
 	// Add foods to vector of foods
-	vector<Food*> foodVec;
-	foodVec.push_back(&banana);
-	foodVec.push_back(&bread);
-	foodVec.push_back(&eggs);
-	foodVec.push_back(&pancakes);
+	vector<unique_ptr<Food>> foodVec;
+	foodVec.push_back(std::move(banana));
+	foodVec.push_back(std::move(bread));
+	foodVec.push_back(std::move(eggs));
+	// foodVec.push_back(&pancakes);
 
 	// Check preparedness of foods, prepare foods, then check again
-	checkPreparedness(foodVec);
-	prepare(foodVec);
-	checkPreparedness(foodVec);
+	foodVec = checkPreparedness(std::move(foodVec));
+	foodVec = prepare(std::move(foodVec));
+	foodVec = checkPreparedness(std::move(foodVec));
 	return 0;
 }
 
 /*!
- * Cooks list of food. 
+ * Cooks list of food using unique pointers.
  * @param itemsToCook List of items to cook.
  */
-void prepare(vector<Food*> itemsToCook) {
-	for (vector<Food*>::iterator i = itemsToCook.begin(); i != itemsToCook.end(); ++i) {
-		(*i) -> cookFood();
+vector<unique_ptr<Food>> prepare(vector<unique_ptr<Food>> itemsToCook) {
+	for (auto& i : itemsToCook) {
+		i -> cookFood();
 	}
+
+	return itemsToCook;
 }
 
 /*!
- * Checks to see if food in list of food is prepared.
+ * Checks to see if food in list of food is prepared using unique pointers.
  * @param itemsToCook List of items to check.
  */
-void checkPreparedness(vector<Food*> itemsToCook) {
-	for (vector<Food*>::iterator i = itemsToCook.begin(); i != itemsToCook.end(); ++i)	 {
-		cout << "Food is cooked: " << (*i) -> isCooked() << endl;
+vector<unique_ptr<Food>> checkPreparedness(vector<unique_ptr<Food>> itemsToCook) {
+	for (auto &i : itemsToCook) {
+		cout << "Food is cooked: " << i -> isCooked() << endl;		
 	}
+
+	return itemsToCook;
 }
